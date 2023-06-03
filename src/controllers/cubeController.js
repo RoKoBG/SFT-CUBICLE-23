dca37da531a6b36a375fe9316e2090dea7f549f5
Rosen Koychev <rosen.koichev@gmail.com>
2023-06-03 18:29:45 +0300

    Add 404 page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,21 +2,23 @@ const router = require("express").Router();
 const cubeService = require("../services/cubeService");
 // Path /cubes/create
 router.get("/create", (req, res) => {
-    console.log(cubeService.getAll());
+  console.log(cubeService.getAll());
   res.render("create");
 });
 
 router.post("/create", (req, res) => {
-
   const { name, description, imageUrl, difficultyLevel } = req.body;
 
   cubeService.create({ name, description, imageUrl, difficultyLevel });
-  
+
   res.redirect("/");
 });
-router.get('/:cubeId/details', (req,res)=>{
+router.get("/:cubeId/details", (req, res) => {
   const cube = cubeService.getOne(req.params.cubeId);
-  res.render('details', {cube})
-})
+  if (!cube) {
+    return res.redirect("/404");
+  }
+  res.render("details", { cube });
+});
 
 module.exports = router;
