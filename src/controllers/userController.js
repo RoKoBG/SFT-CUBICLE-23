b5c05b9061f5ecc85cfc70bce3bed52f66328643
Rosen Koychev <rosen.koichev@gmail.com>
2023-06-09 06:05:49 +0300

    Hash password for save

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
+const userService = require('../services/userService')
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 router.get("/login", (req, res) => {
-    res.render("users/login");
-  });
+  res.render("users/login");
+});
+
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+  await userService.register({username, password, repeatPassword });
+  res.redirect("/users/login");
+});
 module.exports = router;
---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: {
+    type: String,
+    // validate: {
+    //   validator: function (value) {
+    //     return this.repeatPassword === value;
+    //   },
+    //   message: `Password missmatch!`,
+    // },
+  },
+});
+
+userSchema.virtual("repeatPassword").set(function (value) {
+  if (value !== this.password) {
+    throw new mongoose.MongooseError("Password is not matched!");
+  }
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
