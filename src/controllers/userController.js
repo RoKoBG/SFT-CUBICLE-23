462189b5281cb298b9431007d63b0ea9ff6a9543
Rosen Koychev <rosen.koichev@gmail.com>
2023-06-09 06:11:08 +0300

    add login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
-const userService = require('../services/userService')
+const userService = require("../services/userService");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
 
 router.post("/register", async (req, res) => {
   const { username, password, repeatPassword } = req.body;
-  await userService.register({username, password, repeatPassword });
+  await userService.register({ username, password, repeatPassword });
   res.redirect("/users/login");
 });
-module.exports = router;
+
+router.get("/login", (req, res) => {
+  res.render("/users/login");
+});
+router.module.exports = router;
